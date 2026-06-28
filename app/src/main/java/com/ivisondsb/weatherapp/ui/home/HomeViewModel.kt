3ccf3afb8865980166b9f203d0196b4db2bc1c40
Ivison <ivison.dev@gmail.com>
2026-06-28 15:33:30 -0300

    feat: update HomeViewModel to fetch multiple cities

--- app/src/main/java/com/ivisondsb/weatherapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ivisondsb/weatherapp/ui/home/HomeViewModel.kt
@@ -11,13 +11,25 @@ import kotlinx.coroutines.launch
 class HomeViewModel : ViewModel() {
 
     private val repository = WeatherRepository()
+    private val _weatherList = MutableStateFlow<List<WeatherResponse>>(emptyList())
+    val weatherList: StateFlow<List<WeatherResponse>> = _weatherList
 
-    private val _weather = MutableStateFlow<WeatherResponse?>(null)
-    val weather: StateFlow<WeatherResponse?> = _weather
+    private val cities = listOf(
+        Pair("Recife",                  Pair(-8.0476,  -34.8770)),
+        Pair("Olinda",                  Pair(-8.0089,  -34.8553)),
+        Pair("São Paulo",               Pair(-23.5505, -46.6333)),
+        Pair("Rio de Janeiro",          Pair(-22.9068, -43.1729)),
+        Pair("Brasília",                Pair(-15.7801, -47.9292)),
+        Pair("Manaus",                  Pair(-3.1019,  -60.0250)),
+        Pair("Belo Horizonte",          Pair(-19.9191, -43.9386)),
+    )
 
-    fun getWeather(lat: Double, lon: Double) {
+    fun getAllWeather() {
         viewModelScope.launch {
-            _weather.value = repository.getWeather(lat, lon)
+            val results = cities.map { (_, coords) ->
+                repository.getWeather(lat = coords.first, lon = coords.second)
+            }
+            _weatherList.value = results
         }
     }
 }
