1e4d774749cf15265f02d1d9c6ced5a1bc8e2dee
Ivison <ivison.dev@gmail.com>
2026-06-27 21:40:29 -0300

    feat: create a title screen

--- app/src/main/java/com/ivisondsb/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/weatherapp/MainActivity.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ivisondsb.weatherapp.ui.theme.WeatherAppTheme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +43,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherApp(modifier: Modifier = Modifier) {
-    Column(modifier
-        .fillMaxSize()
-        .padding(10.dp)) {
-        Text("It's time... ⛅")
+
+    fun getCurrentDate(): String {
+        val formatter = DateTimeFormatter.ofPattern(
+            "EEE, dd MMM yyyy",
+            Locale.ENGLISH
+        )
+
+        return LocalDate.now().format(formatter)
+    }
+
+    val currentDate = getCurrentDate()
+
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column() {
+                Text("Weather App", fontSize = 30.sp, fontWeight = FontWeight.SemiBold)
+                Text(currentDate, fontSize = 12.sp, color = Color.Gray)
+            }
+            Text("⛅", fontSize = 40.sp)
+        }
+        Spacer(Modifier.height(10.dp))
     }
 }
 
